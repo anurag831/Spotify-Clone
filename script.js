3bc23a6df0652daa1a04478286f73527b1740c87
Anurag <anuragganguly83@gmail.com>
2024-04-17 20:35:43 +0530

    Display Songs in library section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("lets write javascript");
 
-async function main(){
+async function getSongs(){
     let a = await fetch("http://127.0.0.1:3000/songs/");
     let response = await a.text();
     console.log(response);
@@ -14,6 +14,27 @@ async function main(){
             songs.push(element.href);
         }
     }
+    return songs;
+}
+
+async function main(){
+    // Get the list of all the songs
+    let songs = await getSongs();
+    console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song} </li>`;
+    }
+
+
+    //Play the songs
+    var audio = new Audio(songs[1]);
+    // audio.play();
+    
+    audio.addEventListener("loadeddata", ()=>{
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    });
 }
 
 main();
