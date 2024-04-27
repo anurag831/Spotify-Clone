dcee68b71a0accc1d3e093da686fd0fa51cdd4a2
Anurag <anuragganguly83@gmail.com>
2024-04-27 22:37:31 +0530

    Current song can be played and paused

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-console.log("lets write javascript");
+// console.log("lets write javascript");
+let currentSong = new Audio();
 
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
-  console.log(response);
+  // console.log(response);
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
@@ -11,19 +12,27 @@ async function getSongs() {
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-        a = element.href.split("/songs/")[1]
-        b = a.split(".")[0];
+      a = element.href.split("/songs/")[1];
+      b = a.split(".")[0];
       songs.push(b);
     }
   }
   return songs;
 }
 
+const playMusic = (track)=>{
+  // let audio = new Audio("/songs/" + track + ".mp3");
+  currentSong.src = "/songs/" + track + ".mp3"
+  currentSong.play();
+}
+
 async function main() {
+  
+
   // Get the list of all the songs
   let songs = await getSongs();
-  console.log(songs);
 
+  //Show all the songs in the playlist
   let songUL = document
     .querySelector(".songList")
     .getElementsByTagName("ul")[0];
@@ -42,13 +51,28 @@ async function main() {
          </li>`;
   }
 
-  //Play the songs
-  var audio = new Audio(songs[1]);
-  // audio.play();
-
-  audio.addEventListener("loadeddata", () => {
-    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+  //Attach an event listener to each song
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      // console.log(e.getElementsByTagName("div")[0].getElementsByTagName("div")[0].innerHTML);
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    });
   });
+
+  // Attach an event listener to play, previous and next
+  play.addEventListener("click", ()=>{
+    if(currentSong.paused){
+      currentSong.play();
+      play.src = "pause.svg"
+    }
+    else{
+      currentSong.pause();
+      play.src = play.svg
+    }
+  })
 }
 
 main();
